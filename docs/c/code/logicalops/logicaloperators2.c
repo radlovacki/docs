2cea0e9f64051aadf2f0525f4bba92cd5556fd16
Velimir Radlovački <velimir.radlovacki@gmail.com>
2023-05-03 07:07:25 +0200

    desktop

---
+++ docs/c/code/logicalops/logicaloperators2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 2;
+    printf("%d", a && b);
+    return 0;
+}
---
+++ docs/c/code/relationalops/relational1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 2;
+    char c = 'a', d = 'z';
+    float e = 3.14159, f = 3.14159;
+    printf("%d %d %d", a > b, c >= d, e >= f);
+    return 0;
+}
