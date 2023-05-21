2d2253481ce0ed0c2a617d2aa402c954103ef800
Velimir Radlovački <velimir.radlovacki@gmail.com>
2023-05-21 13:15:10 +0200

    Deployed 544dbba with MkDocs version: 1.4.2

---
+++ c/workbook/geometry/paper1.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int visina, sirina;
+    scanf("%d%d", &visina, &sirina);
+    printf("%d", visina * sirina);
+    return 0;
+}
--- cn/code/listen0.c
+++ cn/code/listen0.c
@@ -10,18 +10,23 @@ int main(void)
     int wsaError = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (wsaError == 0)
     {
-        SOCKET example_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (example_socket != INVALID_SOCKET)
+        SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (listen_socket != INVALID_SOCKET)
         {
             SOCKADDR_IN service;
             service.sin_family = AF_INET;
             service.sin_addr.s_addr = inet_addr("127.0.0.1");
             service.sin_port = htons(27015);
-            if (bind(example_socket, (SOCKADDR*)&service, sizeof(service)) != SOCKET_ERROR)
-                printf("bind() OK!\n");
+            if (bind(listen_socket, (SOCKADDR*)&service, sizeof(service)) != SOCKET_ERROR)
+            {
+                if (listen(listen_socket, 1) != SOCKET_ERROR)
+                    printf("listen() success!\n");
+                else
+                    printf("listen() error: %ld.\n", WSAGetLastError());
+            }
             else
                 printf("bind() error: %ld.\n", WSAGetLastError());
-            closesocket(example_socket);
+            closesocket(listen_socket);
         }
         else
             printf("socket() error: %ld.\n", WSAGetLastError());
